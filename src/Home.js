759984dd304282a22213b9ced51ab4d7c5efdf8a
animevn <haanhth21@gmail.com>
2020-03-31 00:10:15 +0700

    change tab with carousel style now, feel good now

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,118 @@
-import React from "react";
-import "bootstrap/js/dist/tab";
+import React, {useEffect, useState} from "react";
+import $ from "jquery";
+import "bootstrap/js/dist/tab"
 import "bootstrap/js/dist/carousel";
 import {HomeContent, ContactContent, ProfileContent} from "./data";
 
 function Home() {
 
+  const [tab, setTab] = useState({tab1: true, tab2: false, tab3: false});
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/babel";
+    script.src = (
+      <script>
+        {
+          $('#carousel').on('slid.bs.carousel', function (event) {
+            event.preventDefault();
+            const id = event.relatedTarget.id;
+            switch (id) {
+              case "pills-home":
+                setTab({tab1: true, tab2: false, tab3: false});
+                break;
+              case "pills-profile":
+                setTab({tab1: false, tab2: true, tab3: false});
+                break;
+              case "pills-contact":
+                setTab({tab1: false, tab2: false, tab3: true});
+                break;
+              default:
+                break;
+            }
+          })
+        }
+      </script>
+    );
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
+  function onHomeClick(event) {
+    event.preventDefault();
+    setTab({tab1: true, tab2: false, tab3: false});
+  }
+
+  function onProfileClick(event) {
+    event.preventDefault();
+    setTab({tab1: false, tab2: true, tab3: false});
+  }
+
+  function onContactClick(event) {
+    event.preventDefault();
+    setTab({tab1: false, tab2: false, tab3: true});
+  }
+
   return (
     <div className="container mt-3 px-0">
 
-      <ul className="nav nav-pills mb-3">
-        <li className="nav-item">
-          <a className="nav-link active" data-toggle="pill" href="#pills-home">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" data-toggle="pill" href="#pills-profile">Profile</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" data-toggle="pill" href="#pills-contact">Contact</a>
-        </li>
-      </ul>
+      <div id="carousel" className="carousel slide tab-content"
+           data-interval="false">
 
-      <div className="tab-content" id="pills-tabContent">
 
-        <div className="tab-pane fade show active text-justify" id="pills-home">
-          {HomeContent}
-        </div>
+        <ul className="nav nav-pills mb-3">
+          <li className="nav-item">
+            <a className={"nav-link " + (tab.tab1 ? "active" : "")}
+               data-toggle="pill" href="#pills-home" onClick={onHomeClick}>
+              Home
+            </a>
+          </li>
+          <li className="nav-item ">
+            <a className={"nav-link " + (tab.tab2 ? "active" : "")}
+               data-toggle="pill" href="#pills-profile" onClick={onProfileClick}>
+              Profile
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className={"nav-link " + (tab.tab3 ? "active" : "")}
+               data-toggle="pill" href="#pills-contact" onClick={onContactClick}>
+              Contact
+            </a>
+          </li>
+        </ul>
 
-        <div className="tab-pane fade text-justify" id="pills-profile">
-          {ProfileContent}
-        </div>
+        <div className="carousel-inner">
 
-        <div className="tab-pane fade text-justify" id="pills-contact">
-          {ContactContent}
+          <div className="carousel-item tab-pane active" id="pills-home">
+            {HomeContent}
+          </div>
+
+          <div className="carousel-item tab-pane" id="pills-profile">
+            {ContactContent}
+          </div>
+
+          <div className="carousel-item tab-pane" id="pills-contact">
+            {ProfileContent}
+          </div>
+
+          <a className="carousel-control-prev" href="#carousel" role="button"
+             data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carousel" role="button"
+             data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+
         </div>
 
       </div>
+
 
     </div>
   )
